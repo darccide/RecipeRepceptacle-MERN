30e1ec8e00ae2efd71516b593773ebe9c7e29d85
Steven Thomson <darccide@protonmail.com>
2019-09-13 23:56:50 -0500

    implement JWT, Custom Auth Middleware, and JWT Verify

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Creator = require('../../models/Creator');
 
 // ROUTE          GET api/auth
 // DESCRIPTION    Test route
 // ACCESS         Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+	try {
+		const creator = await Creator.findById(req.creator.id).select('-password');
+		res.json(creator);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
--- routes/api/creators.js
+++ routes/api/creators.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
-const Creator = require('../../models/creator');
+const Creator = require('../../models/Creator');
 
 // ROUTE          POST api/creators
 // DESCRIPTION    Test route
@@ -52,7 +54,16 @@ router.post(
 
 			await creator.save();
 
-			res.send('Creator Registered');
+			const payload = {
+				creator: {
+					id: creator.id,
+				},
+			};
+
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('Server error');
