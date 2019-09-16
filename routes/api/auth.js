39a6c01c695b13fe5cc888ace51beaf33e9880b8
Steven Thomson <darccide@protonmail.com>
2019-09-15 19:30:35 -0500

    Everything to Create and Update Profile Routes added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const Creator = require('../../models/Creator');
 
-// ROUTE          GET api/auth
-// DESCRIPTION    Test route
-// ACCESS         Public
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
 router.get('/', auth, async (req, res) => {
 	try {
 		const creator = await Creator.findById(req.creator.id).select('-password');
@@ -16,5 +20,49 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
+router.post(
+	'/',
+	[check('email', 'Please include a valid email').isEmail(), check('password', 'Password is required').exists()],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let creator = await Creator.findOne({ email });
+
+			if (!creator) {
+				return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+			}
+
+			const isMatch = await bcrypt.compare(password, creator.password);
+
+			if (!isMatch) {
+				return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+			}
+
+			const payload = {
+				creator: {
+					id: creator.id,
+				},
+			};
+
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+				if (err) throw err;
+				res.json({ token });
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	}
+);
 
 module.exports = router;
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,9 +1,109 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// ROUTE          GET api/profiles
-// DESCRIPTION    Test route
-// ACCESS         Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const Creator = require('../../models/Creator');
+
+// ROUTE          GET api/profiles/me
+// DESCRIPTION    Get current creators profile
+// ACCESS         Private
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ creator: req.creator.id }).populate('creator', ['name', 'avatar']);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this creator' });
+		}
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// ROUTE          POST api/profiles
+// DESCRIPTION    Create or update creator profile
+// ACCESS         Private
+router.post(
+	'/',
+	[
+		auth,
+		[
+			check('status', 'Status is required')
+				.not()
+				.isEmpty(),
+			check('skills', 'Skills is required')
+				.not()
+				.isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			company,
+			website,
+			location,
+			bio,
+			status,
+			skills,
+			youtube,
+			facebook,
+			twitter,
+			instagram,
+			linkedin,
+		} = req.body;
+
+		// Build profile object
+		const profileFields = {};
+		profileFields.creator = req.creator.id;
+		if (company) profileFields.company = company;
+		if (website) profileFields.website = website;
+		if (location) profileFields.location = location;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (skills) {
+			profileFields.skills = skills.split(',').map(skill => skill.trim());
+		}
+
+		// Build social object
+		profileFields.social = {};
+		if (youtube) profileFields.youtube = youtube;
+		if (facebook) profileFields.facebook = facebook;
+		if (twitter) profileFields.twitter = twitter;
+		if (instagram) profileFields.instagram = instagram;
+		if (linkedin) profileFields.linkedin = linkedin;
+
+		try {
+			let profile = await Profile.findOne({ creator: req.creator.id });
+
+			if (profile) {
+				// Update
+				profile = await Profile.findOneAndUpdate(
+					{ creator: req.creator.id },
+					{ $set: profileFields },
+					{ new: true }
+				);
+				// Return Profile
+				return res.json(profile);
+			}
+
+			// Create
+			profile = new Profile(profileFields);
+			// Save Profile
+			await profile.save();
+			// Return profile
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 module.exports = router;
